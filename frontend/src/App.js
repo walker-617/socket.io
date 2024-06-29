25ac632673bdceac7cf0c01e3761cb7cd74169e2
WALKER <walker.1100100@gmail.com>
2024-06-29 16:07:31 +0530

    added loading

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 function App() {
   return (
     <div>
-      <div style={{backgroundColor:"black", color:"white",padding:"5px",marginBottom:"10px", fontSize:"10px"}}>Note: Since this is deployed on a free service, the server will spin down after 15 minutes of inactivity. For more details, check <a href="https://docs.render.com/free#:~:text=Render%20spins%20down,will%20hang%20temporarily." target="_blank"  style={{color:"white"}}>here</a>.</div>
+      <div style={{backgroundColor:"black", color:"white",padding:"5px",marginBottom:"10px", fontSize:"10px"}}>Note: Since this is deployed on a free service, the server will spin down after 15 minutes of inactivity and spin up again when a new request comes. This causes the loading to take some time. For more details, check <a href="https://docs.render.com/free#:~:text=Render%20spins%20down,will%20hang%20temporarily." target="_blank"  style={{color:"white"}}>here</a>.</div>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<CreateEnterRoom/>}/>
--- frontend/src/components/CreateEnterRoom.jsx
+++ frontend/src/components/CreateEnterRoom.jsx
@@ -9,6 +9,20 @@ function CreateEnterRoom() {
   const idRef = useRef();
   const [error, setError] = useState("");
 
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(`${serverURL}/isServerOn`)
+      .then((res) => res.text())
+      .then((ack) => {
+        if (ack === "yes") {
+          setLoading(false);
+        } else {
+          setLoading(true);
+        }
+      });
+  }, []);
+
   function createRoom() {
     setError("");
     const roomId = getRandomID();
@@ -20,7 +34,9 @@ function CreateEnterRoom() {
           return;
         }
         socket.emit("createRoom", roomId);
-        navigate(`/room/${roomId}`, { state: { role: "host", roomId: roomId } });
+        navigate(`/room/${roomId}`, {
+          state: { role: "host", roomId: roomId },
+        });
       });
   }
 
@@ -39,66 +55,33 @@ function CreateEnterRoom() {
           return;
         }
         socket.emit("enterRoom", roomId);
-        navigate(`/room/${roomId}`, { state: { role: "guest", roomId: roomId } });
+        navigate(`/room/${roomId}`, {
+          state: { role: "guest", roomId: roomId },
+        });
       });
   }
 
   return (
-    <div style={{margin: "10px"}}>
-    <br/>
-      <button onClick={() => createRoom()}>Create Room?</button>
-      <br/><br/>
+    <div style={{ margin: "10px" }}>
+      <br />
+      <button onClick={() => createRoom()} disabled={loading}>Create Room?</button>
+      <br />
+      <br />
       <div>Enter Room?</div>
-      <input ref={idRef} type="text" placeholder="Enter Room ID" style={{padding:"5px"}}/>{" "}
-      <button onClick={() => enterRoom()} style={{padding:"5px"}}>Enter</button>
+      <input
+        ref={idRef}
+        type="text"
+        placeholder="Enter Room ID"
+        style={{ padding: "5px" }}
+        disabled={loading}
+      />{" "}
+      <button onClick={() => enterRoom()} style={{ padding: "5px" }} disabled={loading}>
+        Enter
+      </button>
       {error ? <div style={{ color: "red" }}>{error}</div> : null}
+      {loading ? <div style={{ color: "orange",marginTop:"20px" }}>Loading... <br></br>This might take a while.</div> : null}
     </div>
   );
 }
 
 export default CreateEnterRoom;
-
-// import socket from "../utils/Socket";
-// import { useNavigate } from "react-router-dom";
-// import { getRandomID } from "../utils/functions";
-// import { useRef, useState } from "react";
-
-// function CreateEnterRoom() {
-//   const navigate = useNavigate();
-//   const idRef = useRef();
-//   const [error, setError] = useState("");
-
-//   function createRoom() {
-//     setError("");
-//     const roomId = getRandomID();
-//     socket.emit("createRoom", roomId);
-//     if (!error) {
-//       navigate(`/room/${roomId}`, { state: { role: "host", roomId: roomId } });
-//     }
-//   }
-
-//   function enterRoom() {
-//     setError("");
-//     const roomId = idRef.current.value;
-//     if (!roomId) {
-//       setError("Please enter a room ID");
-//       return;
-//     }
-//     socket.emit("enterRoom", roomId);
-//     if (!error) {
-//       navigate(`/room/${roomId}`, { state: { role: "guest", roomId: roomId } });
-//     }
-//   }
-
-//   return (
-//     <div>
-//       <button onClick={() => createRoom()}>Create Room?</button>
-//       <div>Enter Room?</div>
-//       <input ref={idRef} type="text" placeholder="Enter Room ID" />
-//       <button onClick={() => enterRoom()}>Enter</button>
-//       {error ? <div style={{ color: "red" }}>{error}</div> : null}
-//     </div>
-//   );
-// }
-
-// export default CreateEnterRoom;
